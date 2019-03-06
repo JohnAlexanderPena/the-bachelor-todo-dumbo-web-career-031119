ed9c270ab2c44e62ceeffcb8f6ed021fe3459855
John Pena <jpena91@gmail.com>
2019-03-06 03:35:07 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |bio|
+    if bio["status"].downcase == "winner"
+      return bio["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-end
+  data.each do |season_num, array_of_hashes|
+  array_of_hashes.each do |bio|
+    if bio["occupation"] == occupation
+      return bio["name"]
+    end 
+  end 
+end 
+end 
+  
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season_num, array_of_hashes|
+    array_of_hashes.each do |bio,stats|
+      bio.each do |names_age, info|
+        if info == hometown
+          counter += 1 
+        end 
+      end 
+    end 
+  end 
+  counter 
 end
 
-def get_occupation(data, hometown)
-  # code here
+  def get_occupation(data, hometown)
+  occupation = []
+  data.each do |season_num,array_of_hashes|
+    array_of_hashes.each do |bio,stats|
+      bio.each do |names_age,info|
+         if info == hometown 
+          return bio["occupation"]
+        end 
+      end 
+      end 
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+  answer=[]
+  data[season].each do |season_data| 
+      answer.push(season_data.values_at("age").join.to_f)
+    end   
+  return (answer.inject {|a,b| a + b} / answer.length).round
+ end
+
