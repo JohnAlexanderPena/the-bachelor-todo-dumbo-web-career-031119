8be5d76f1a44b5d5ababf54a6db9860705fe166b
John Pena <jpena91@gmail.com>
2019-03-09 19:47:24 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,7 +52,6 @@ def get_average_age_for_season(data, season)
   data[season].each do |season_data| 
       total.push(season_data.values_at("age").join.to_f)
     end 
-  return (total.inject{ |sum, el| sum + el }.to_f / total.size)
-  binding.pry .round
+  return (total.inject{ |sum, el| sum + el }.to_f / total.size).round
  end
 
