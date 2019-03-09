b8ef4210baeb0ae10324fd91cf2706b7ec8106bf
John Pena <jpena91@gmail.com>
2019-03-09 19:34:03 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,6 +52,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |season_data| 
       total.push(season_data.values_at("age").join.to_f)
     end   
-  total = 
+  avg = total.each do |i|
+  avg += i
  end
 
